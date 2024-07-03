b8a069796c7310a3427cda80ef867ff1efd9b234
castrokjoseph <castrojoseph10@gmail.com>
2024-07-03 21:59:35 +0530

    done

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const API_URL_KEY = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    matic: {
+      url: API_URL_KEY,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
